3c0c370ebf6ac055261f582a580fe1896c10ba1c
Fatih Karahan <hseyin97@gmail.com>
2020-08-02 23:37:52 +0300

    Updates

---
+++ precache-manifest.c7f099eeebf4a956dc27634ef9f41847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d0b52b728eb37edc23ad95c23593f6",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "970d8527789b97b3c1cb",
+    "url": "/crwn-clothing/static/css/main.f327d78a.chunk.css"
+  },
+  {
+    "revision": "fefb996c8142d892acde",
+    "url": "/crwn-clothing/static/js/2.22ad7514.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crwn-clothing/static/js/2.22ad7514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970d8527789b97b3c1cb",
+    "url": "/crwn-clothing/static/js/main.01b0e002.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  }
+]);
